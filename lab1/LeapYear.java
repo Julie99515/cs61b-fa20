86d7de4cec9649967e4b150451a20ff98390ebad
Julie99515 <1173040083@qq.com>
2021-04-14 12:07:29 +0800

    second part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,7 +2,25 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
-
+    /** Make judegements if the given year is leap year
+     * 
+     * @param year
+     */
+    public static boolean isLeapYear(int year) {
+        if(year % 400 == 0) {
+            return true;
+        } else {
+            if(year % 4 == 0) {
+                if(year % 100 != 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
